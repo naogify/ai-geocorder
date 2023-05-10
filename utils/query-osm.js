9c9c09c8981cffab96f810d1702ec223c13a633d
naoki <n.globe.us@gmail.com>
2023-05-10 17:38:52 +0900

    some fix

--- utils/query-osm.js
+++ utils/query-osm.js
@@ -60,24 +60,30 @@ const queryOverpass = async (query) => {
 
 export const queryOsmData = async (input) => {
 
+  // OpenAIで市区町村名を抽出
   const addressesResult = await requestOpenAI(`次の入力文から市区町村名を抽出して下さい。入力文:${input}`)
   
+  // 改行と記号を削除
   const address = addressesResult.data.choices[0].text.replace(/\n/g, '').replace(/[^\w\sぁ-んァ-ン一-龯]|_/g, "");
+
+  // 都道府県名と市区町村名を取得
   const normalized = await normalize(address)
 
   if (!normalized.pref || !normalized.city) {
     throw new Error("市区町村名を入力して下さい。");
   }
-
+  // 都道府県名と、市区町村名を APIに投げて行政ポリゴンを取得
   const url = `https://naogify.github.io/japanese-admins/${normalized.pref}/${normalized.city}.json`
   const cityResponse = await fetch(url)
   const json = await cityResponse.json()
-
   const bbox = turf.bbox(json)
   
+  // バウンディングボックスと入力文を OpenAI に渡して Overpass API のクエリを生成
   const queryResponse = await requestOpenAI(CHAT_TEMPLATE(input, [bbox[1], bbox[0], bbox[3], bbox[2]]))
+  // バッククォート内のクエリを取得
   const query = queryResponse.data.choices[0].text.match(/```([\s\S]*?)```/g)[0].replace(/```/g, '')
 
+  // Overpass API にクエリを投げて結果を取得
   const response = await queryOverpass(query)
 
   const geojson = {
