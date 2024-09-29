b5cba4b6f416b279b260d0ac84cc570b7e4e53eb
naoki <n.globe.us@gmail.com>
2024-09-29 14:20:56 +0900

    gpt-4o-miniにモデルを変更

--- utils/query-osm.js
+++ utils/query-osm.js
@@ -1,41 +1,40 @@
-import { Configuration, OpenAIApi } from "openai";
+import OpenAI from "openai";
 import fetch from "node-fetch";
 import * as turf from '@turf/turf'
 import { normalize } from "@geolonia/normalize-japanese-addresses";
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
-const CHAT_TEMPLATE = (human_input, bbox) => {
+const createPromptForOSMQuery = (bbox) => {
 
   return `Assistant is an expert OpenStreetMap Overpass API assistant.
-
-  For each question that the user supplies, the assistant will reply with:
-  (1) A statement consenting to help.
-  (2) The text of a valid Overpass API query that can be used to answer the question. The query should be enclosed by three backticks on new lines, denoting that it is a code block. The area of the query should be specified by the following bounding box: ${bbox}.
-
-  Assistant has a whimsical personality. Assistant will reply with a geospatial themed joke or a pun if the user asks a question that is not relevant to the Overpass API.
-
-  {history}
-  Human: ${human_input}
-  Assistant:
-  `
+  For each question that the user supplies, the assistant will reply with: The text of a valid Overpass API query that can be used to answer the question.
+  The query should be enclosed by three backticks on new lines, denoting that it is a code block.
+  The area of the query should be specified by the following bounding box: ${bbox}.`
 }
 
-const requestOpenAI = async (CHAT_TEMPLATE) => {
+const requestOpenAI = async (humanInput, systemInput, responseFormat) => {
 
-  const openai = new OpenAIApi(configuration);
+  const openai = new OpenAI();
 
-  return await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: CHAT_TEMPLATE,
-    temperature: 0,
-    max_tokens: 516,
+  const completion = await openai.chat.completions.create({
+    model: "gpt-4o-mini",
+    temperature: 0, // 回答を一定にする
+    max_tokens: 2048,
     top_p: 1,
     frequency_penalty: 0,
-    presence_penalty: 0
+    presence_penalty: 0,
+    response_format: {
+      "type": responseFormat || "text"
+    },
+    messages: [
+      { role: "system", content: systemInput },
+      {
+        role: "user",
+        content: humanInput,
+      },
+    ],
   });
+
+  return completion.choices[0].message.content;
 }
 
 const OVERPASS_API_URL = "https://overpass-api.de/api/interpreter";
@@ -61,30 +60,42 @@ const queryOverpass = async (query) => {
 export const queryOsmData = async (input) => {
 
   // OpenAIで市区町村名を抽出
-  const addressesResult = await requestOpenAI(`次の入力文から市区町村名を抽出して下さい。入力文:${input}`)
-  
+  const userInput = `次の入力文から市区町村名を抽出して下さい。入力文:${input}`
+  const systemPromptForGetCity = "与えられた入力文から市区町村名を抽出して下さい。レスポンスは市区町村名のみとして下さい。記号等も必要ありません。"
+  const cityRaw = await requestOpenAI(userInput, systemPromptForGetCity)
+
   // 改行と記号を削除
-  const address = addressesResult.data.choices[0].text.replace(/\n/g, '').replace(/[^\w\sぁ-んァ-ン一-龯]|_/g, "");
+  const city = cityRaw.replace(/\n/g, '').replace(/[^\w\sぁ-んァ-ン一-龯]|_/g, "");
 
   // 都道府県名と市区町村名を取得
-  const normalized = await normalize(address)
+  const normalized = await normalize(city)
 
-  if (!normalized.pref || !normalized.city) {
-    throw new Error("市区町村名を入力して下さい。");
+  if (!normalized.pref) {
+    throw new Error("都道府県名が分かりませんでした");
   }
+
+  if (!normalized.city) {
+    throw new Error("市区町村名が分かりませんでした");
+  }
+
   // 都道府県名と、市区町村名を APIに投げて行政ポリゴンを取得
   const url = `https://naogify.github.io/japanese-admins/${normalized.pref}/${normalized.city}.json`
   const cityResponse = await fetch(url)
   const json = await cityResponse.json()
   const bbox = turf.bbox(json)
-  
+
+  if (!bbox) {
+    throw new Error("市区町村名が正しくありません。");
+  }
+
   // バウンディングボックスと入力文を OpenAI に渡して Overpass API のクエリを生成
-  const queryResponse = await requestOpenAI(CHAT_TEMPLATE(input, [bbox[1], bbox[0], bbox[3], bbox[2]]))
+  const promptForOSMQuery = createPromptForOSMQuery(bbox)
+  const queryRaw = await requestOpenAI(userInput, promptForOSMQuery);
   // バッククォート内のクエリを取得
-  const query = queryResponse.data.choices[0].text.match(/```([\s\S]*?)```/g)[0].replace(/```/g, '')
+  const query = queryRaw.match(/```([\s\S]*?)```/g)[0].replace(/```/g, '')
 
   // Overpass API にクエリを投げて結果を取得
-  const response = await queryOverpass(query)
+  const response = await queryOverpass(query);
 
   const geojson = {
     type: 'FeatureCollection',
@@ -93,8 +104,8 @@ export const queryOsmData = async (input) => {
 
   for (const element of response.elements) {
     if (
-      element.type === 'node' && 
-      element.tags && 
+      element.type === 'node' &&
+      element.tags &&
       element.tags.hasOwnProperty('name')
     ) {
       geojson.features.push({
